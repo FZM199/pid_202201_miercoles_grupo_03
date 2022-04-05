5141a2feef5e75f89e2a318dd892cc30ee3895eb
fzapatam99 <fzapatam99@gmail.com>
2022-04-05 14:24:56 -0500

    Primer Avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDepartamentoComponent } from './components/add-departamento/add-departamento.component';
+import { AddIncidenteComponent } from './components/add-incidente/add-incidente.component';
+import { AddPropietarioComponent } from './components/add-propietario/add-propietario.component';
+import { AddServiciosComponent } from './components/add-servicios/add-servicios.component';
+import { AddUsuarioComponent } from './components/add-usuario/add-usuario.component';
+import { AddVisitanteComponent } from './components/add-visitante/add-visitante.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"addUsuario",component:AddUsuarioComponent },
+  {path:"addPropietario",component:AddPropietarioComponent},
+  {path:"addIncidente",component:AddIncidenteComponent},
+  {path:"addServicio",component:AddServiciosComponent},
+  {path:"addVisitante",component:AddVisitanteComponent},
+  {path:"addDepartamento",component:AddDepartamentoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
