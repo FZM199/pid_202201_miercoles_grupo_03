da47c2552c80428bb11417c66a741860350bc2f7
FZM199 <fezami@hotmail.com>
2022-04-26 19:13:35 -0500

    solu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './auth/login.component';
+import { AddDepartamentoComponent } from './components/add-departamento/add-departamento.component';
+import { AddMascotaComponent } from './components/add-mascota/add-mascota.component';
+import { AddPropietarioComponent } from './components/add-propietario/add-propietario.component';
+import { AddVisitanteComponent } from './components/add-visitante/add-visitante.component';
+import { AddIncidenteComponent } from './components/add-incidente/add-incidente.component';
+import { AddServiciosComponent } from './components/add-servicios/add-servicios.component';
 
 
 @NgModule({
@@ -16,6 +22,12 @@ import { LoginComponent } from './auth/login.component';
     MenuComponent,
     LoginComponent,
     IndexComponent,
+    AddDepartamentoComponent,
+    AddMascotaComponent,
+    AddPropietarioComponent,
+    AddVisitanteComponent,
+    AddIncidenteComponent,
+    AddServiciosComponent
   ],
   imports: [
     BrowserModule,
